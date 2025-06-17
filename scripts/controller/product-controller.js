8c648657a296a9a2c8a64b519572fb8e3b4ef511
Diya Singh <rajdiya.456@gmail.com>
2025-06-17 19:11:07 +0530

    feat: create initial DOM structure

--- scripts/controller/product-controller.js
+++ scripts/controller/product-controller.js
@@ -6,3 +6,33 @@ async function loadPizzas(){
    console.log('Pizzas are', pizza);
 }
 loadPizzas();
+
+function preparePizzaCard(){
+   const outputDiv = document.querySelector('#output');
+   const colDiv = document.createElement('div');
+   colDiv.className = 'col-4';
+   const cardDiv = document.createElement('div');
+   cardDiv.className = 'card';
+   cardDiv.style = "width: 18rem;";
+   colDiv.appendChild(cardDiv);
+   const img = document.createElement('img');
+   img.src = '';
+   img.className = 'card-img-top';
+   cardDiv.appendChild(img);
+   const cardBody = document.createElement('div');
+   cardBody.className = 'card-body';
+   cardDiv.appendChild(cardBody);
+   const h5 = document.createElement('h5');
+   h5.className = 'card-title';
+   h5.innerText = 'Card title';
+   const pTag = document.createElement('p');
+   pTag.className = 'card-text';
+   pTag.innerText = 'Some Text...';
+   const button = document.createElement('button');
+   button.innerText = 'Add to Cart';
+   button.className = 'btn btn-primary';
+   cardBody.appendChild(h5);
+   cardBody.appendChild(pTag);
+   cardBody.appendChild(button);
+   outputDiv.appendChild(colDiv);
+}
